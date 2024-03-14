b4ef4102fcfa6ed2c2188b55bd50d6366ffc668b
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2024-03-14 15:21:19 -0300

    Add project files.

---
+++ Temperatura.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Temperatura.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-- Conversor de Fahrenheit para Celsius  --");
+
+            Console.WriteLine("Digite o valor da temperatura em Fahrenheit: ");
+            double temperatura = double.Parse(Console.ReadLine());
+
+            double temperaturaFinal = 5 / 9 * (temperatura - 32);
+
+            Console.WriteLine("")
+        }
+    }
+}
---
+++ VolumeConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿// Ex 01: Crie um programa para calcular o volume de uma caixa retangular
+using System.Globalization;
+
+namespace VolumeConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-- Calculadora de Volume --");
+            
+            Console.WriteLine("Digite o tamanho da altura: ");
+            double altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite o tamanho da largura: ");
+            double largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite o tamanho do comprimento: ");
+            double comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double volume = altura * largura * comprimento;
+
+            Console.WriteLine("Volume: " + volume.ToString("F2"), CultureInfo.InvariantCulture);
+        }
+    }
+}
