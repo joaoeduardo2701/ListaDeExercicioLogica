3a926dc8311d75fbbb155c0f7f7e59cd2cfce622
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2024-03-20 14:36:44 -0300

    exercício 13 finalizado

---
+++ ex13.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ex13.ConsoleApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a quantidade de pães vendidos: ");
+        int paes = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a quantidade de broas vendidas: ");
+        int broas = int.Parse(Console.ReadLine());
+
+        double arrecadado = (paes * 0.12) + (broas * 1.50);
+        double poupanca = arrecadado * 0.1;
+
+        Console.WriteLine("Valor arrecadado: " + arrecadado);
+        Console.WriteLine("Valor que vai para a poupança: " + poupanca);
+    }
+}
