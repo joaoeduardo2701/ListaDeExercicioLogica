b285f9e544ab1218a653c5cfda79c72699a4f5bc
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2024-03-22 14:49:33 -0300

    Mudança nas pastas

---
+++ ex14.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ex14.ConsoleApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("-- Quantos dias de vida você tem? --");
+
+        Console.WriteLine("Digite o seu nome: ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Digite a sua idade");
+        int idade = int.Parse(Console.ReadLine());
+
+        int diasDeIdade = idade * 365;
+
+        Console.WriteLine($"{nome} você já viveu {diasDeIdade} dias!");
+    }
+}
---
+++ ex16.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ex16.ConsoleApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("-- Aumento de salários --");
+
+        Console.WriteLine("Digite o salário do funcionário: ");
+        double salario = double.Parse(Console.ReadLine());
+
+        double salarioFinal = (salario * 1.15) - (salario * 1.15 * 0.08);
+
+        Console.WriteLine("Salário final do funcionário é R$" + salarioFinal.ToString("f2"));
+    }
+}
